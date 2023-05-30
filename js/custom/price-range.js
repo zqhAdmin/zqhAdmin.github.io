7fe4d64797a087f8449f2cd79e8753e5c0dbd09a
zqhAdmin <112857475+zqhAdmin@users.noreply.github.com>
2023-05-30 11:00:03 +0800

    Add files via upload

---
+++ js/custom/price-range.js
@@ -0,0 +1,1 @@
+$((function(){$("#slider-range").slider({range:!0,min:0,max:500,values:[75,300],slide:function(c,b){$("#amount").val("$"+b.values[0]+" - $"+b.values[1])}}),$("#amount").val("$"+$("#slider-range").slider("values",0)+" - $"+$("#slider-range").slider("values",1))}));
