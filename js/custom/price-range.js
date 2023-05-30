ba221fe032f549854235e91947718d74e946c3e9
zqhAdmin <112857475+zqhAdmin@users.noreply.github.com>
2023-05-30 11:02:02 +0800

    Delete js directory

--- js/custom/price-range.js
+++
@@ -1,1 +0,0 @@
-$((function(){$("#slider-range").slider({range:!0,min:0,max:500,values:[75,300],slide:function(c,b){$("#amount").val("$"+b.values[0]+" - $"+b.values[1])}}),$("#amount").val("$"+$("#slider-range").slider("values",0)+" - $"+$("#slider-range").slider("values",1))}));
